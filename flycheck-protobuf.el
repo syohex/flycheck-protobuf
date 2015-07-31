97198f95fc4a34a01c7afc424c29963b8cc21419
Edward Knyshov <edvorg@gmail.com>
2015-07-31 10:56:24 +0300

    fix keywords

--- flycheck-protobuf.el
+++ flycheck-protobuf.el
@@ -6,7 +6,7 @@
 ;; Created: 31 July 2015
 ;; Version: 0.9
 ;; Package-Requires: ((protobuf-mode "1.0"))
-;; Keywords: dotemacs startup speed config package
+;; Keywords: flycheck
 ;; X-URL: https://github.com/edvorg/flycheck-protobuf
 ;; This program is free software; you can redistribute it and/or
 ;; modify it under the terms of the GNU General Public License as
